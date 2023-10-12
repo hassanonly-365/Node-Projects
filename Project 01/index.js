dd50de1679ef7a470aa807f624ff20f40c3e0631
Hassan <hassanwaqar205@outlook.com>
2023-10-12 21:45:54 +0500

    Add files via upload

---
+++ Project 01/index.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+import chalk from 'chalk';
+const generatedNo = Math.floor(Math.random() * 20);
+let answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "Write your Guess:"
+    }
+]);
+let { userGuess } = answer;
+console.log("UserGuess:", userGuess);
+console.log("Generated No:", generatedNo);
+if (userGuess === generatedNo) {
+    console.log((chalk.blueBright("You Guessed the Correct Number.\nYou Won")));
+}
+else {
+    console.log(chalk.redBright(chalk.bgBlack("Better Luck next time!")));
+}
